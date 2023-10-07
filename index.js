6c196ece0c058bb41141b4429d34339ed3b70b10
Brandi Burnell <brandiburnell6@gmail.com>
2023-10-06 21:02:27 -0600

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(driversNames, name) {
+    //let lowercaseDriversNames = driversNames.map(driver => driver.toLowerCase());
+    //console.log(lowercaseDriversNames);
+    const matchingNames = driversNames.filter(item => item.toLowerCase() === name.toLowerCase());
+    return matchingNames;
+}
+
+function fuzzyMatch(driversNames, letters) {
+    const matchingNames = driversNames.filter(name => name.substring(0,2) === letters);
+    return matchingNames;
+}
+
+function matchName(drivers, person) {
+    const matchingName = drivers.filter(driver => driver.name === person);
+    return matchingName;
+}
